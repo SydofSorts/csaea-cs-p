d536ff6f4b485c904f7be3a6582df5c90670e756
SydofSorts <sydneychackossc@gmail.com>
2026-09-08 18:22:25 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,35 @@
+# comment
+# Trick: highlight all text you want as comments, use ctrl + / to turn all text to comments
+
+print("Hello World!")
+
+# VARIABLE DECLARATIONS & DATA TYPES: 
+
+a = 4        # integer (Whole Number)
+b = 5.5      # float   (Decimal)
+c = "CSAEA"  # string  ("letter[s]")
+d = True     # boolean (True or False)
+
+print(a, b, c, d)
+
+# OPERATORS
+
+# + (addition)
+# - (subtraction)
+# * (multiplication)
+# / (division)
+
+# %  (modulus a.k.a remainder)
+# ** (to the power of)
+# // (division rounded to the nearest whole number)
+
+# += (add value to variable and updates its value)
+# -= (subtract value from variable and updates its value)
+# /= (divides value from variable and updates its value)
+
+e = 3 - 1
+e /= 2
+
+# F-STRING
+
+print(f"e is NOW equal to {e}")
