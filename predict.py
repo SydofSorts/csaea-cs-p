0cb6f3a9026cea246be0d9bc823afa8452412fa8
SydofSorts <sydneychackossc@gmail.com>
2026-09-18 18:23:41 +0000

    hellloooo

---
+++ predict.py
@@ -0,0 +1,10 @@
+import math
+
+
+print(7 // 2, 7 % 2, -7 // 2)
+print(int(-5.9), math.floor(-5.9))
+print("5" * 3, "5" + "5")
+print(2 ** 4, math.pow(2, 4))
+print(True + True + True)
+print(0.1 + 0.2 == 0.3)
+print("Zebra" < "apple")
