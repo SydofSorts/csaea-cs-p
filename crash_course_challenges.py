023deafe3884a0cc33683fa4d2daec4df3db2841
SydofSorts <sydneychackossc@gmail.com>
2026-09-24 17:26:24 +0000

    hii

---
+++ crash_course_challenges.py
@@ -0,0 +1,23 @@
+import math
+
+# CHALLENGE 1: TIP CALCULATOR
+
+bill = 50
+tip = bill * 0.2
+total = tip + bill 
+
+print(f"Tip:{tip}\nTotal:{total}" )
+
+# CHALLENGE 11: ROCKET LAUNCH 
+
+start = 10
+
+ 
+for integer in range(start, 0, -1):
+   print(integer)
+
+if integer == 1:
+   print("Liftoff!")
+
+
+
