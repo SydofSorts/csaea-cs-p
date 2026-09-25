f3f30835fffafbb26c9aed9226a364589593a829
SydofSorts <sydneychackossc@gmail.com>
2026-09-25 18:23:23 +0000

    eee

--- crash_course_challenges.py
+++ crash_course_challenges.py
@@ -12,12 +12,34 @@ print(f"Tip:{tip}\nTotal:{total}" )
 
 start = 10
 
- 
 for integer in range(start, 0, -1):
    print(integer)
 
 if integer == 1:
    print("Liftoff!")
+
+# CHALLENG #4: REPORT CARD
+
+score = 50
+
+if score >= 90:
+   print("A")
+elif score < 90 and score >= 80:
+   print("B")
+elif score < 80 and score >= 70:
+   print("C")
+elif score < 70 and score >= 60:
+   print("D")
+else:
+   print("F")
+
+# CHALLENGE #15: CLASS PASS RATE
+
+
+
+
+
+
 
 
 
